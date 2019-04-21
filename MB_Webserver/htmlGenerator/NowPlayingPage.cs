822336b1056ab380a192b511a852233f4f449e15
Avik Biswas <avik.avikbiswas@gmail.com>
2019-04-22 04:12:46 +0530

    New stuff

--- MB_Webserver/htmlGenerator/NowPlayingPage.cs
+++ MB_Webserver/htmlGenerator/NowPlayingPage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using MusicBeePlugin.IhtmlGenerator;
 
 namespace MusicBeePlugin.htmlGenerator
@@ -7,7 +8,25 @@ namespace MusicBeePlugin.htmlGenerator
 	{
 		public string GetGeneratedResponse()
 		{
-			throw new NotImplementedException();
+			StringBuilder sb = new StringBuilder();
+
+			sb.Append(
+				$@"        
+		<div class='song-info' id='song-info'>
+			<div class='overlay'>
+                <div class='artwork'>
+                    <img id = 'current-track-artwork' class='current-track-artwork' src='/img/artwork-default.png'>
+                </div>
+                <div class='trackinfo'>
+                    <h1 id = 'title-big' class='track-title'>Track Title</h1>
+                    <p><span id = 'artist-big' class='artist'>Artist name</span><span id = 'album-big' class='album'>Album name</span></p>
+					<p class='artistinfo' id='artistInfo'></p>
+                </div>
+            </div>
+        </div>"
+				);
+
+			return sb.ToString();
 		}
 	}
 }
